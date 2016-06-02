871645a25c5970aa915e33addf07ce3dfe402ad6
Carol Yao <c.y-@hotmail.com>
2016-06-02 15:18:10 -0400

    Added to deliver method to account for even or odd sides of house

--- paperboy.rb
+++ paperboy.rb
@@ -14,5 +14,30 @@ attr_writer :name, :experience, :side
     "#{ 50 + (experience/2.0) }"
   end
 
+    def deliver(starting_address, end_address)
+      if side == "even"
+        if starting_address.even? && end_address.even?
+          "#{((end_address - starting_address) / 2) + 1 }"
+        elsif starting_address.odd? && end_address.odd?
+          "#{(end_address - starting_address) / 2}"
+        elsif starting_address.odd? && end_address.even?
+          "#{(end_address - (starting_address - 1)) / 2}"
+        elsif starting_address.even? && end_address.odd?
+          "#{((end_address + 1) - starting_address) / 2}"
+        end
+      elsif side == "odd"
+        if starting_address.odd? && end_address.odd?
+          "#{((end_address - starting_address) / 2) + 1}"
+        elsif starting_address.even? && end_address.even?
+          "#{(end_address - starting_address) / 2}"
+        elsif starting_address.odd? && end_address.even?
+          "#{(end_address - (starting_address - 1)) / 2}"
+        elsif starting_address.odd? && end_address.odd?
+          "#{((end_address + 1) - starting_address) / 2}"
+        end
+      else
+        "side not inputted properly"
+      end
+    end
 
 end
