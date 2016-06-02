8210b7a1716a973edc9c0c1e3a75ab2b7564a32f
Carol Yao <c.y-@hotmail.com>
2016-06-02 17:09:56 -0400

    Added questions for user input

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,3 +1,18 @@
+puts "What is the plateau size?"
+plateau_size = gets.chomp
+
+puts "What is the initial position and header for the first rover?"
+initial_position_header_1 = gets.chomp
+
+puts "What are the series of moves for the first rover?"
+moves = gets.chomp
+
+puts "What is the initial position and header for the second rover?"
+initial_position_header_2 = gets.chomp
+
+puts "What are the series of moves for the second rover?"
+moves_2 = gets.chomp
+
 class Rover
 
   def initialize(x_coordinate, y_coordinate, direction)
@@ -5,5 +20,8 @@ class Rover
     @y_coordinate = y_coordinate
     @direction = direction
   end
-  
+
+  def read_instruction(instruction)
+  end
+
 end
