eb83de72be07e5384c37e6bfd7ad86d5c9ff529b
Carol Yao <c.y-@hotmail.com>
2016-06-02 16:56:33 -0400

    Fixed output from Strings to Number values

--- paperboy.rb
+++ paperboy.rb
@@ -11,18 +11,18 @@ attr_writer :name, :experience, :side
   end
 
   def quota
-    "#{ 50 + (experience / 2.0) }"
+    50 + (experience / 2.0)
   end
 
     def deliver(starting_address, end_address)
       if (side == "even" && starting_address.even? && end_address.even?) || (side == "odd" && starting_address.odd? && end_address.odd?)
-          "#{amount_paid(((end_address - starting_address) / 2) + 1)}"
+          amount_paid(((end_address - starting_address) / 2) + 1)
       elsif (side == "even" && starting_address.odd? && end_address.odd?) || (side == "odd" && starting_address.even? && end_address.even?)
-          "#{amount_paid((end_address - starting_address) / 2)}"
+          amount_paid((end_address - starting_address) / 2)
       elsif (side == "even" && starting_address.odd? && end_address.even?) || (side == "odd" && starting_address.odd? && end_address.even?)
-          "#{amount_paid((end_address - (starting_address - 1)) / 2)}"
+          amount_paid((end_address - (starting_address - 1)) / 2)
       elsif (side == "even" && starting_address.even? && end_address.odd?) || (side == "odd" && starting_address.odd? && end_address.odd?)
-          "#{amount_paid(((end_address + 1) - starting_address) / 2)}"
+          amount_paid(((end_address + 1) - starting_address) / 2)
       else
         "side not inputted properly"
       end
