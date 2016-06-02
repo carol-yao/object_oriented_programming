2d763210cfc99edc27e6f9fbef2361809e41f720
Carol Yao <c.y-@hotmail.com>
2016-06-02 16:54:14 -0400

    Fixed delivery bug that would rewrite before running

--- paperboy.rb
+++ paperboy.rb
@@ -3,7 +3,7 @@ class Paperboy
 attr_reader :name, :experience, :side, :earnings
 attr_writer :name, :experience, :side
 
-  def initialize(name, experience, side, earnings)
+  def initialize(name, side, earnings = 0, experience = 0)
     @name = name
     @experience = experience
     @side = side
@@ -11,46 +11,35 @@ attr_writer :name, :experience, :side
   end
 
   def quota
-    "#{ 50 + (experience/2.0) }"
+    "#{ 50 + (experience / 2.0) }"
   end
 
     def deliver(starting_address, end_address)
-      if side == "even"
-        if starting_address.even? && end_address.even?
+      if (side == "even" && starting_address.even? && end_address.even?) || (side == "odd" && starting_address.odd? && end_address.odd?)
           "#{amount_paid(((end_address - starting_address) / 2) + 1)}"
-        elsif starting_address.odd? && end_address.odd?
+      elsif (side == "even" && starting_address.odd? && end_address.odd?) || (side == "odd" && starting_address.even? && end_address.even?)
           "#{amount_paid((end_address - starting_address) / 2)}"
-        elsif starting_address.odd? && end_address.even?
+      elsif (side == "even" && starting_address.odd? && end_address.even?) || (side == "odd" && starting_address.odd? && end_address.even?)
           "#{amount_paid((end_address - (starting_address - 1)) / 2)}"
-        elsif starting_address.even? && end_address.odd?
+      elsif (side == "even" && starting_address.even? && end_address.odd?) || (side == "odd" && starting_address.odd? && end_address.odd?)
           "#{amount_paid(((end_address + 1) - starting_address) / 2)}"
-        end
-      elsif side == "odd"
-        if starting_address.odd? && end_address.odd?
-          "#{amount_paid(((end_address - starting_address) / 2) + 1)}"
-        elsif starting_address.even? && end_address.even?
-          "#{amount_paid((end_address - starting_address) / 2)}"
-        elsif starting_address.odd? && end_address.even?
-          "#{amount_paid((end_address - (starting_address - 1)) / 2)}"
-        elsif starting_address.odd? && end_address.odd?
-          "#{amount_paid(((end_address + 1) - starting_address) / 2)}"
-        end
       else
         "side not inputted properly"
       end
     end
 
     def amount_paid(total_houses)
-      puts experience
-      puts total_houses
-      @experience += total_houses
-      if total_houses > quota.to_i
-        (quota.to_i * 0.25) + ((total_houses - quota.to_i) * 0.50)
-      elsif total_houses == quota.to_i
-        quota.to_i * 0.25
-      else
-        (total_houses * 0.25) - 2
+
+      amount =  if total_houses > quota.to_i
+                  (quota.to_i * 0.25) + ((total_houses - quota.to_i) * 0.50)
+                elsif total_houses == quota.to_i
+                  quota.to_i * 0.25
+                else
+                  (total_houses * 0.25) - 2
       end
+      @experience += total_houses
+      @earnings += amount
+      amount
     end
 
     def report
@@ -58,7 +47,3 @@ attr_writer :name, :experience, :side
     end
 
 end
-
-sam = Paperboy.new("Sam", 100, "even", 20)
-
-puts sam.deliver(1, 20)
