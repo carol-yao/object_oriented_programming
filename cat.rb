2ab286e7b67ca09fe4ec9225674a0ccc9275572b
Carol Yao <c.y-@hotmail.com>
2016-06-02 14:16:40 -0400

    Added instance method eats_at to retrieve meal_time in 12 h clock format

--- cat.rb
+++ cat.rb
@@ -1,11 +1,23 @@
 class Cat
 
+  attr_reader :meal_time
+
   def initialize(name, preferred_food, meal_time)
     @name = name
     @preferred_food = preferred_food
     @meal_time = meal_time
   end
 
+  def eats_at
+    if meal_time < 12
+      "#{ meal_time } AM"
+    elsif meal_time == 12
+      "#{ meal_time } PM"
+    else
+      "#{ meal_time - 12} PM"
+    end
+  end
+  
 end
 
 fuzzy = Cat.new('fuzzy', 'fish', 2)
