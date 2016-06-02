a280f6c598899872a7259a7a5dd1a71cf7a627ba
Carol Yao <c.y-@hotmail.com>
2016-06-02 17:25:29 -0400

    Added read_instruction method to decide if rover should move or turn

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,27 +1,33 @@
 puts "What is the plateau size?"
-plateau_size = gets.chomp
+plateau_size = gets.chomp.scan(/\w+/)
 
 puts "What is the initial position and header for the first rover?"
-initial_position_header_1 = gets.chomp
+initial_position_header_1 = gets.chomp.scan(/\w+/)
 
 puts "What are the series of moves for the first rover?"
-moves = gets.chomp
+moves = gets.chomp.scan(/\w+/)
 
 puts "What is the initial position and header for the second rover?"
-initial_position_header_2 = gets.chomp
+initial_position_header_2 = gets.chomp.scan(/\w+/)
 
 puts "What are the series of moves for the second rover?"
-moves_2 = gets.chomp
+moves_2 = gets.chomp.scan(/\w+/)
 
 class Rover
 
+attr_accessor :x_coordinate, :y_coordinate, :direction
   def initialize(x_coordinate, y_coordinate, direction)
     @x_coordinate = x_coordinate
     @y_coordinate = y_coordinate
     @direction = direction
   end
 
-  def read_instruction(instruction)
+  def read_instruction
+    if initial_position_header_1[0] != x_coordinate || initial_position_header_1[1] != y_coordinate || initial_position_header_1[2] != direction
+      move
+    else
+      turn
+    end
   end
 
 end
