3523a0c9d58afe9249bd6cf84c45e33b9c1acfb0
Carol Yao <c.y-@hotmail.com>
2016-06-03 14:57:09 -0400

    Added Receipt class

--- sales_taxes.rb
+++ sales_taxes.rb
@@ -1,5 +1,7 @@
 class Item
 
+  attr_accessor :type
+
   def initialize(quantity, type, name, price)
     @quantity = quantity
     @type = type
@@ -20,24 +22,42 @@ class Item
   end
 
   def calculate_tax
-    if @type == imported good
+    if type == "imported good"
       sales_tax_imported + sales_tax_good
-    elsif @type == good
+    elsif type == "good"
       sales_tax_good
-    elsif @type == imported
+    elsif type == "imported"
       sales_tax_imported
-    elsif @type == book || @type == food || @type == medical
+    elsif type == "book" || type == "food" || type == "medical"
       sales_tax_exempt
+    else
+      "invalid type"
     end
   end
 
+end
 
-
-
+class Receipt
+  def initialize
+    @items = []
+  end
 
 end
 
-choco = Item.new(1, "choco", "choco", 5)
-puts choco.sales_tax_good
-puts choco.sales_tax_exempt
-puts choco.sales_tax_imported
+# choco = Item.new(1, "imported good", "choco", 5)
+# puts choco.calculate_tax
+#
+# fish = Item.new(1, "good", "choco", 5)
+# puts fish.calculate_tax
+#
+# cracker = Item.new(1, "imported", "choco", 5)
+# puts cracker.calculate_tax
+#
+# book = Item.new(1, "book", "choco", 5)
+# puts book.calculate_tax
+#
+# food = Item.new(1, "food", "choco", 5)
+# puts food.calculate_tax
+#
+# med = Item.new(1, "medical", "choco", 5)
+# puts med.calculate_tax
