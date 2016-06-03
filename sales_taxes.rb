76f0c4af6aef11e000b646c3b213adb998791e95
Carol Yao <c.y-@hotmail.com>
2016-06-03 15:41:42 -0400

    Fixed add method to keep track of total sales tax and total price

--- sales_taxes.rb
+++ sales_taxes.rb
@@ -1,6 +1,6 @@
 class Item
 
-  attr_accessor :type
+  attr_accessor :type, :sales_tax, :price
 
   def initialize(quantity, type, name, price)
     @quantity = quantity
@@ -23,13 +23,13 @@ class Item
 
   def calculate_tax
     if type == "imported good"
-      sales_tax_imported + sales_tax_good
+      sales_tax = sales_tax_imported + sales_tax_good
     elsif type == "good"
-      sales_tax_good
+      sales_tax = sales_tax_good
     elsif type == "imported"
-      sales_tax_imported
+      sale_tax = sales_tax_imported
     elsif type == "book" || type == "food" || type == "medical"
-      sales_tax_exempt
+      sales_tax = sales_tax_exempt
     else
       "invalid type"
     end
@@ -38,48 +38,53 @@ class Item
 end
 
 class Receipt
+
+  attr_accessor :total_sales_tax, :total
   def initialize
     @items = []
+    @total_sales_tax = 0
+    @total = 0
   end
 
-  @@sales_tax = 0
-  @@total = 0
+
 
   def add(item)
     if item.is_a?(Item)
       @items << item
-      @@sales_tax += calculate_tax(item)
-      @@total += calculate_tax(item) + @price
+      @total_sales_tax += item.calculate_tax
+      @total += item.calculate_tax + item.price
     else
       "That is not of class Item."
     end
   end
 
-  def self.total_sales_tax
-    @@sales_tax
-  end
-
-  def self.total
-    @@total
-  end
-  
 
 end
 
-# choco = Item.new(1, "imported good", "choco", 5)
+
+
+
+
+choco = Item.new(1, "imported good", "choco", 5)
 # puts choco.calculate_tax
-#
-# fish = Item.new(1, "good", "choco", 5)
+
+fish = Item.new(1, "good", "choco", 5)
 # puts fish.calculate_tax
-#
-# cracker = Item.new(1, "imported", "choco", 5)
+
+cracker = Item.new(1, "imported", "choco", 5)
 # puts cracker.calculate_tax
-#
-# book = Item.new(1, "book", "choco", 5)
+
+book = Item.new(1, "book", "choco", 5)
 # puts book.calculate_tax
-#
-# food = Item.new(1, "food", "choco", 5)
+
+food = Item.new(1, "food", "choco", 5)
 # puts food.calculate_tax
-#
-# med = Item.new(1, "medical", "choco", 5)
-# puts med.calculate_tax
+
+med = Item.new(1, "medical", "choco", 5)
+ # puts med.calculate_tax
+
+receipt1 = Receipt.new
+p receipt1.add(choco)
+p receipt1
+puts receipt1.total_sales_tax
+puts receipt1.total
