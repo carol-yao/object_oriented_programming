e28ff2e0112287b046051739690c9303d5ab451a
Carol Yao <c.y-@hotmail.com>
2016-06-03 14:39:43 -0400

    Added sales tax methods for regular goods, exempted and imported

--- sales_taxes.rb
+++ sales_taxes.rb
@@ -1,5 +1,7 @@
 class Item
 
+  attr_reader :price
+
   def initialize(quantity, type, name, price)
     @quantity = quantity
     @type = type
@@ -7,4 +9,23 @@ class Item
     @price = price
   end
 
+  def sales_tax_goods
+    ((10.00 * price) / 100.00)
+  end
+
+  def sales_tax_exempt
+    0
+  end
+
+  def sales_tax_imported
+    ((5.00 * price) / 100.00)
+  end
+
+
+
 end
+
+choco = Item.new(1, "choco", "choco", 5)
+puts choco.sales_tax_goods
+puts choco.sales_tax_exempt
+puts choco.sales_tax_imported
